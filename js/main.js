fb6ecede9d8cfa5637e747d017d21e56cfbeac99
Adrian Melo <amelo2@fvi.edu>
2017-06-20 22:04:45 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
   $("#button").click(function () {
-      if ($("#calc").val() <= "2")
+      if ($("#calc").val() <= "0.8%")
       {
       $("#ok").html("<p> YOU CAN DRIVE SAVE</p>");
     }else{
-      $("#ok").html("<p>Estas preso</p>");
+      $("#ok").html("<p>You can't drive for 4 hours</p>");
     }
   });
