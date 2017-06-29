4a2dcbd9cabcc6002ba2c23135c763b241f84c8f
Adrian Melo <amelo2@fvi.edu>
2017-06-28 21:32:06 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-  $("#button").click(function () {
-      if ($("#calc").val() <= "0.8%")
-      {
-      $("#ok").html("<p> YOU CAN DRIVE SAVE</p>");
-    }else{
-      $("#ok").html("<p>You can't drive for 4 hours</p>");
-    }
-  });
+$("#button").click(function ()
+{
+  //creating variables
+  var drinkCount =Number( $("#calc").val());
+  var weight = Number($("#weight").val());
+  var sexType= $("#select-gender").val();
+  var percSexType=0;
+
+  if (sexType == "male"){
+    percSexType=40;
+  }else {
+    percSexType=50;
+  }
+
+  //calculatig the percentage of alchohol
+  var dangerPerc=((drinkCount*percSexType)/(weight*10)).toFixed(2);
+
+  var limitOk=0.03;
+  var limitMaybe=0.07;
+
+//showing results
+    if (dangerPerc <= limitOk)
+    {
+      
+      $("#ok").addClass("answer");
+
+  } else if(dangerPerc>limitOk && dangerPerc <= limitMaybe){
+
+  }else{
+
+  }
+});
